a826afc6a835b7063937704156d512a271eace74
amtune <allisontune@gmail.com>
2023-08-09 19:53:20 -0400

    server file

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require("path")
+const fs = require("fs")
+const dataBase = require ("./db/db.json")
+
+const PORT = process.env.PORT || 3001;  
+const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));  
+app.use(express.static('public'));  
+//connects to index file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
