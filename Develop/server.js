21db98c8b6baedd37b733e0fa14ddd53ea3f5e15
amtune <allisontune@gmail.com>
2023-08-09 19:54:17 -0400

    deletes notes

--- Develop/server.js
+++ Develop/server.js
@@ -27,3 +27,29 @@ app.post('/api/notes', (req, res) => {
     const newNote = createNote(req.body, dataBase)
     res.json(newNote)
 })
+
+// Creates notes
+const createNote = (body, notesArr) => {
+    const newNote = body;
+    if (!Array.isArray(notesArr)) {
+        notesArr = [];
+    }
+    if(notesArr.length === 0) {
+        notesArr.push(0);
+    }
+
+    body.id = notesArr.length;
+    notesArr[0]++
+    notesArr.push(newNote)
+    
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArr, null, 2)
+    )
+    return newNote
+}
+//Deletes old note
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, dataBase);
+    res.json(true);
+})
