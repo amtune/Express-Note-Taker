ec32a9fb31c25406d0ece02d8abd11e663aa213a
amtune <allisontune@gmail.com>
2023-08-09 19:53:48 -0400

    connect to notes

--- Develop/server.js
+++ Develop/server.js
@@ -13,3 +13,17 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
+
+//Connects to notes file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(dataBase.slice(1))
+})
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNote(req.body, dataBase)
+    res.json(newNote)
+})
